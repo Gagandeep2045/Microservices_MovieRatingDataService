89e6a72be6373b1731bb94aed75e30d28eb55132
GagandeepBudhiraja <gagandeep.singh2045@gmail.com>
2019-11-03 23:42:55 +0530

    Distributed log tracing using sleuth

--- src/main/java/javabrains/io/ratingDataService/controller/RatingController.java
+++ src/main/java/javabrains/io/ratingDataService/controller/RatingController.java
@@ -3,6 +3,8 @@ package javabrains.io.ratingDataService.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,8 +17,11 @@ import javabrains.io.ratingDataService.model.MovieRating;
 @RequestMapping("/rating")
 public class RatingController {
 
+	private Logger logger=LoggerFactory.getLogger(getClass());
+
 	@RequestMapping(method=RequestMethod.GET)
 	public List<MovieRating> getRatingForUser(@RequestParam(name = "user",required=false) String user) {
+		logger.info("Delegating request to service class");
 		if (user!=null && user.equalsIgnoreCase("gagan"))
 			return Arrays.asList(new MovieRating("M101", 5), new MovieRating("M103", 2));
 		return Arrays.asList(new MovieRating("M101", 4), new MovieRating("M102", 5), new MovieRating("M103", 1));
